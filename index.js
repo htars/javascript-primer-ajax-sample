8ab270bedbbd4edbb83a8d251fa2a451bc57c714
Yohei Hiyama <hymyhds@gmail.com>
2020-05-23 22:10:15 +0900

    update function

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
-function fetchUserInfo(userId) {
-  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
-    .then((response) => {
-      console.log(response.status);
-      if (!response.ok) {
-        console.error("エラーレスポンス", response);
-      } else {
-        return response.json().then((userInfo) => {
-          // HTMLの組み立て
-          const view = escapeHTML`
-          <h4>${userInfo.name} (@${userInfo.login})</h4>
-          <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-          <dl>
-            <dt>Location</dt>
-            <dd>${userInfo.location}</dd>
-            <dt>Repository</dt>
-            <dd>${userInfo.public_repos}</dd>
-          </dl>
-          `;
+async function main() {
+  try {
+    const userId = getUserId();
+    const userInfo = await fetchUserInfo(userId);
+    const view = createView(userInfo);
+    displayView(view);
+  } catch (error) {
+    console.error(`エラーが発生しました(${error})`);
+  }
+}
 
-          // HTMLの挿入
-          const result = document.getElementById("result");
-          result.innerHTML = view;
-        });
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+function fetchUserInfo(userId) {
+  return fetch(
+    `https://api.github.com/users/${encodeURIComponent(userId)}`
+  ).then((response) => {
+    if (!response.ok) {
+      return Promise.reject(
+        new Error(`${response.status}: ${response.statusText}`)
+      );
+    } else {
+      return response.json();
+    }
+  });
+}
+
+function getUserId() {
+  const value = document.getElementById("userId").value;
+  return encodeURIComponent(value);
+}
+
+function createView(userInfo) {
+  return escapeHTML`
+    <h4>${userInfo.name} (@${userInfo.login})</h4>
+    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+    <dl>
+      <dt>Location</dt>
+      <dd>${userInfo.location}</dd>
+      <dt>Repository</dt>
+      <dd>${userInfo.public_repos}</dd>
+    </dl>
+    `;
+}
+
+function displayView(view) {
+  const result = document.getElementById("result");
+  result.innerHTML = view;
 }
 
 function escapeSpecialChars(str) {
